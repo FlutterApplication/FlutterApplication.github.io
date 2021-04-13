82f533eb5db23dd4601c363a2cf5297762c22381
Aman Negi <37607224+AmanNegi@users.noreply.github.com>
2021-04-13 11:37:20 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -12,8 +12,8 @@ const RESOURCES = {
 "favicon.png": "72319b9a7adaac2cf5c3d6f417ca9751",
 "icons/Icon-192.png": "868f3d6d258ebdd751a086408d686dd1",
 "icons/Icon-512.png": "d52f4e5dfc942111f4da7b3f1b45a0eb",
-"index.html": "d0a323af2d09bb12c6cff41445aa326c",
-"/": "d0a323af2d09bb12c6cff41445aa326c",
+"index.html": "95d4bc9161ab404a2d3d98a34b2fd1bd",
+"/": "95d4bc9161ab404a2d3d98a34b2fd1bd",
 "main.dart.js": "b78e27b1e8881b8411ef2900751e01ba",
 "manifest.json": "5a2c0526f1fe73e6f211bc7a9d15c210",
 "version.json": "426313f2f3133c2f20415344c4a22df3"
